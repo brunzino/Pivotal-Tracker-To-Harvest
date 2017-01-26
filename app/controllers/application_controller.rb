ef4c1d693109b4bfc84fc09b06cf52c932e8f25c
Nick Bruns <bruns.nicholas@gmail.com>
2017-01-25 23:38:57 -0700

    Grab important data from params and log it

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,9 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
-  protect_from_forgery with: :exception, unless: -> { request.format.json? }
+  # http://api.rubyonrails.org/classes/ActionController/RequestForgeryProtection/ClassMethods.html
+  # protect_from_forgery unless: -> { request.format.json? } # disables CSRF protection for all json requests
+
+  protect_from_forgery with: :exception
 
 end
--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,10 +1,20 @@
 class PagesController < ApplicationController
 
+	skip_before_action :verify_authenticity_token, only: [:accept_request]
+
 	def welcome
 	end
 
 	def accept_request
-		puts "Occurred at #{params[:occurred_at]}"
+
+		@kind = params[:kind]
+		@highlight = params[:highlight]
+		@project_name = params[:project][:name];
+		@story_name = params[:primary_resources][0][:name];
+		@pt_user_id = params[:performed_by][:id];
+		@pt_user_name = params[:performed_by][:name];
+		logger.info "Received request from Pivotal Tracker: Action: #{@kind} - #{@highlight}\n\t Project name: #{@project_name}, Story name: #{@story_name}, User name: #{@pt_user_name} (#{@pt_user_id})"
+
 		redirect_to :action => :welcome
 	end
 	
